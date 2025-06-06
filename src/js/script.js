7fda8b4c879d9abcc6202bfd3f42c8932a8f1952
moycana <oficina@transmoycana.com.br>
2025-06-06 16:20:46 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -165,7 +165,7 @@ textoArea.addEventListener('input', gerarEtiqueta);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("/Editor-de-Etiquetas/service-worker.js")
       .then(reg => console.log("Service Worker registrado:", reg.scope))
       .catch(err => console.log("Erro ao registrar Service Worker:", err));
   });
